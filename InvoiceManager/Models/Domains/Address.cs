8c4c0818d9e9c5c2551f8965ac12812fd5415c11
mmakselon <magdalena.pachacz@gmail.com>
2024-10-05 20:57:33 +0200

    new classes

---
+++ InvoiceManager/Models/Domains/Address.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace InvoiceManager.Models.Domains
+{
+    public class Address
+    {
+
+        public Address()
+        {
+            Clients = new Collection<Client>();
+        }
+
+        public ICollection<Client> Clients { get; set; }
+    }
+}
