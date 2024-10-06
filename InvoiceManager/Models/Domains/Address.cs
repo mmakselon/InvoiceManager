689f3fd37accb0a1c3e824ac0d1e0961962b8b84
mmakselon <magdalena.pachacz@gmail.com>
2024-10-06 21:16:25 +0200

    register

--- InvoiceManager/Models/Domains/Address.cs
+++ InvoiceManager/Models/Domains/Address.cs
@@ -16,15 +16,19 @@ namespace InvoiceManager.Models.Domains
         public int Id { get; set; }
 
         [Required]
+        [Display(Name = "Ulica")]
         public string Street { get; set; }
 
         [Required]
+        [Display(Name = "Numer")]
         public string Number { get; set; }
 
         [Required]
+        [Display(Name = "Miejscowość")]
         public string City { get; set; }
 
         [Required]
+        [Display(Name = "Kod pocztowy")]
         public string PostalCode { get; set; }
 
         public ICollection<Client> Clients { get; set; }
