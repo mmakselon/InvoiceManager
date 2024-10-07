aa1aaf3f633942c26023b7cbef295d40ddb026cd
mmakselon <magdalena.pachacz@gmail.com>
2024-10-07 12:07:33 +0200

    fix

--- InvoiceManager/Controllers/HomeController.cs
+++ InvoiceManager/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InvoiceManager.Models.Domains;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,29 @@ namespace InvoiceManager.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var invoices = new List<Invoice>
+            {
+                new Invoice
+                {
+                    Id=1,
+                    Title="Fa/01/2024",
+                    CreateDate=DateTime.Now,
+                    PaymentDate=DateTime.Now,
+                    Value=999,
+                    Client=new Client {Name="Klient 1"}
+                },
+                new Invoice
+                {
+                    Id=1,
+                    Title="Fa/02/2024",
+                    CreateDate=DateTime.Now,
+                    PaymentDate=DateTime.Now,
+                    Value=12999,
+                    Client=new Client {Name="Klient 2"}
+                },
+
+            };
+            return View(invoices);
         }
 
         public ActionResult Invoice()
