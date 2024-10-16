a634b97f1ff70493fb87ded24c2750aea785c567
mmakselon <magdalena.pachacz@gmail.com>
2024-10-16 21:36:55 +0200

    implement PrintController

--- InvoiceManager/Controllers/PrintController.cs
+++ InvoiceManager/Controllers/PrintController.cs
@@ -1,9 +1,9 @@
-﻿using InvoiceManager.Models.Repositories;
+﻿using InvoiceManager.Models.Domains;
+using InvoiceManager.Models.Repositories;
 using Microsoft.AspNet.Identity;
+using Rotativa;
+using Rotativa.Options;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace InvoiceManager.Controllers
@@ -13,12 +13,39 @@ namespace InvoiceManager.Controllers
     {
         private InvoiceRepository _invoiceRepository = new InvoiceRepository();
 
-        public ActionResult InvoiceTemplate(int id)
+        public ActionResult InvoiceToPdf(int id)
         {
+            var handle = Guid.NewGuid().ToString();
             var userId = User.Identity.GetUserId();
             var invoice = _invoiceRepository.GetInvoice(id, userId);
 
-            return View(invoice);
+            TempData[handle] = GetPdfContent(invoice);
+
+            return Json(new
+            {
+                FileGuid = handle,
+                FileName = $@"Faktura_{invoice.Id}.pdf"
+            });
+        }
+
+        private byte[] GetPdfContent(Invoice invoice)
+        {
+            var pdfResult = new ViewAsPdf(@"InvoiceTemplate", invoice)
+            {
+                PageSize = Size.A4,
+                PageOrientation = Orientation.Portrait
+            };
+
+            return pdfResult.BuildFile(ControllerContext);
+        }
+
+        public ActionResult DownloadInvoicePdf(string fileGuid, string fileName)
+        {
+            if (TempData[fileGuid] == null)
+                throw new Exception("Błąd przy próbie eksportu faktury do PDF.");
+
+            var data = TempData[fileGuid] as byte[];
+            return File(data, "application/pdf", fileName);
         }
     }
 }
